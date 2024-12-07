5196b9476602ff5d8bce5d419577372c492c4013
mussaratshamsher <musaratskhan@github.com>
2024-12-07 12:46:47 +0500

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,99 @@
-import Image from "next/image";
+import Image from 'next/image'
+import React from 'react'
+
+import hero from '../../public/images/hero.png'
+import heroicon from '../../public/images/heroicon.png'
+import heroiconbar from '../../public/images/heroiconbar.png'
+
+
+import {  ArrowLeft, ArrowRight, ChevronRight, MoveRight,  } from 'lucide-react'
+import FlashSales from '@/components/theme/FlashSales'
+import Category from '@/components/theme/Category'
+import SellingProducts from '@/components/theme/SellingProducts'
+import MusicCategory from '@/components/theme/MusicCategory'
+import Products from '@/components/theme/Products'
+import NewArrival from '@/components/theme/NewArrival'
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+
+<div className='w-full h-auto'>
+
+    <div className='grid md:grid-cols-4 grid-cols-1 grid-rows-2 md:h-[400px] h-[450px] w-full md:mb-32'>
+    
+    <div className='grid md:grid-cols-1'>
+  <div className=' md:p-10 md:pr-16 md:text-lg md:border-r-2 md:border-h  ml-10'> 
+<div className='flex md:mt-2'>Men's Fashion <ChevronRight /></div>
+<div className='flex md:mt-2 '>Woman's Fashion <ChevronRight /> </div>
+<div className='md:mt-2'>Electronics</div>
+<div  className='md:mt-2'>Home & lifestyle</div>
+<div className='md:mt-2'>Medicine</div>
+<div className='md:mt-2'>Sports & Outdoor</div>
+<div className='md:mt-2'>Baby's & Toys</div>
+<div className='md:mt-2'>Medicine</div>
+<div className='md:mt-2'>Groceries & Pets</div>
+<div className='md:mt-2'>Health & Beauty</div>
     </div>
-  );
+</div>
+<div className=' grid md:col-span-3  bg-black md:h-[370px] h-[250px] mt-5 md:ml-5 md:mr-10 '>
+
+<div className='grid grid-cols-2'>
+
+  <div className='text-white flex flex-col md:ml-14 ml-4'>
+    <div className=' flex'>
+  <Image src={heroicon} alt='mobileapp' width={2000} height={2000} className='w-14 mt-10 '/> 
+<p className='md:mt-16 mt-5 ml-3'>iPhone 14 Series</p> </div>
+<h1 className='font-bold md:text-5xl text-2xl md:mt-8 mt-4'>Upto 10% </h1>
+<h1 className='font-bold md:text-5xl text-2xl md:mt-4 mt-2 '>off Voucher</h1>
+<h2 className='underline flex flex-row text-lg md:mt-5 mt-2'>Shop Now <MoveRight className='ml-2 mt-1'/></h2>
+  </div>
+ <Image src={hero} alt='mobile' width={500} height={500} className='h-[250px]'/> 
+<Image src={heroiconbar} alt='' width={100} height={100} className='md:ml-[350px] md:mt-10 -mt-5 ml-28'/>
+
+</div>
+</div>
+
+    </div>
+
+
+
+<div className='md:-mt-14 mt-14'>
+  <div className='bg-red-500 w-5 h-10 rounded md:ml-32 ml-5 mb-3'> 
+    <h1 className='md:ml-10 ml-7 text-red-500 mt-7'>Today's </h1>
+  </div>
+  <div className='grid grid-cols-1 md:grid-cols-4'>
+    <h1 className='text-3xl font-bold md:mt-8 md:ml-32 ml-5'>Flash Sales</h1>
+    <div className='grid grid-cols-4 grid-rows-2 md:ml-10 ml-2'>
+      <h1>Days</h1>
+      <h1>Hours</h1>
+       <h1>Minutes</h1>
+       <h1>Seconds</h1>
+       <h1 className='md:text-3xl text-2xl font-bold'>03 <span className='text-red-500'>:</span></h1>
+       <h1 className='md:text-3xl text-2xl font-bold'>23 <span className='text-red-500'>:</span></h1>
+       <h1 className='md:text-3xl text-2xl font-bold'>19 <span className='text-red-500'>:</span></h1>
+       <h1 className='md:text-3xl text-2xl font-bold'>56 <span className='text-red-500'>:</span></h1>
+       
+    </div>
+ <h1></h1>
+    <h1 className='flex md:ml-20  ml-24 gap-3 mt-5 mb-10 '><ArrowLeft className='rounded-full w-10 h-10' id='sale'/> 
+    <ArrowRight className='rounded-full w-10 h-10' id='sale'/></h1>
+  </div>
+  <FlashSales />
+
+</div>
+<Category />
+
+<SellingProducts/>
+
+<MusicCategory />
+
+<Products/>
+
+<NewArrival />
+
+    </div>
+  )
 }
